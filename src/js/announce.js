b1c7a02debf10bdf3e343a850162bfe2ccfd8559
Conor Hughes <conorhughes93@gmail.com>
2017-10-03 20:08:25 +0100

    Dismiss alerts Merge themes with main css

--- src/js/announce.js
+++ src/js/announce.js
@@ -1,40 +1,51 @@
 function Announce(settings_global) {
-    var globalDefaults = {
+    var defaults = {
         position: 'bottom-right',
         theme: 'default',
+        type: 'notice',
         timeout: false,
-        timeoutDelay: 5000
+        timeoutDelay: 5000,
+        dismiss: false
     };
 
-    var settings = $.extend({}, globalDefaults, settings_global);
+    var globalSettings = $.extend({}, defaults, settings_global);
 
     this.init = function () {
         $('body').append($('<ul/>', {
             id: 'announce',
-            class: 'announce ' + 'announce-theme-' + settings.theme + ' ' + settings.position
+            class: 'announce ' + 'announce-theme-' + globalSettings.theme + ' announce-' + globalSettings.position
         }));
     };
 
     this.post = function (settings_message) {
-        var postDefaults = {
-            type: 'notice'
-        };
-
-        var settings = $.extend(globalDefaults, postDefaults, settings_message);
+        var settings = $.extend({}, defaults, settings_message);
 
         $(function () {
+            var dismiss = $('<a/>', {
+                class: 'announce-dismissible',
+                html: '&times'
+            });
+
             var announcement = $('<li/>', {
                 class: 'announce-' + settings.type,
                 text: settings.message
             });
 
-            $('#announce').append(announcement);
+            if (settings.dismiss) {
+                announcement.append(dismiss);
+
+                dismiss.click(function() {
+                    announcement.remove();
+                });
+            }
 
             if (settings.timeout) {
                 setTimeout(function () {
                     announcement.remove();
                 }, settings.timeoutDelay);
             }
+
+            $('#announce').append(announcement);
         });
     };
 
